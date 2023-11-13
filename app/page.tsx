1a75b029e8cb0d93237596d4d7f5813c57eba5db
Elvis Ibarra <ielvisd@gmail.com>
2023-11-13 15:01:24 -0800

    Add first type

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-"use client"; // This is a client component 👈🏽
+// The following line is needed to make this a client component
+"use client";
 
+// Import useState hook, only available in client components
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
   return (
     <div>
       <p>count: {count}</p>
